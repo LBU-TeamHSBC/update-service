a22a18cbd588515b87ac82585ecbc56de1984a61
Ed Brook <edbrook@gmail.com>
2018-02-28 12:25:37 +0000

    Pull linked account details from DB

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const github = require('./adapters/github');
 
 // Mapping needs to be stored in DB
 const provider_map = {
-    342: github
+    1: github
 };
 
 const getDataForUser = (provider_id, user_id, auth_token) => {
     const promise = provider_map[provider_id](user_id, auth_token);
     return promise;
+};
+
+const processData = (student_id, data) => {
+    console.log(student_id + ": " + JSON.stringify(data) + "\n");
 };
 
 const run = _ => {
@@ -33,25 +37,26 @@ const updateUserData = _ => {
             return;
         }
     });
-    // TODO - Query DB for users with linked accounts
-    // TODO - Get last updated for each service
-    // TODO - Update as required
 
-    conn.query("SELECT * FROM user", (err, results, fields) => {
-        results.forEach(user => {
-            console.log(user.id + " " + user.username + " " + user.email);
-        });
-    });
+    // const linked_accounts_sql = "\
+    //     SELECT sv.student_id, sv.oauth_token, v.id AS vendor_id, v.category \
+    //         FROM student_vendor AS sv \
+    //         INNER JOIN vendor v ON v.id=sv.vendor_id";
+    const linked_accounts_sql = "\
+    SELECT u.username, sv.student_id, sv.oauth_token, v.id AS vendor_id, v.category \
+        FROM student_vendor AS sv \
+        INNER JOIN student s ON s.id=sv.student_id \
+        INNER JOIN user u ON u.id=s.user_id \
+        INNER JOIN vendor v ON v.id=sv.vendor_id";
     
-    // Testing...
-    const user_list = [
-        'userA', 'userB', 'userC', 'userD', 'userE', 'userF',
-        'userG', 'userH', 'userI', 'userJ', 'userK', 'userL'
-    ];
-    user_list.forEach(user => {
-        getDataForUser(342, user, 'IABT8ATYLBYCLYRCA8BRLWGALWIYLW8YRWYL8CRUAWB8Y==')
-            .then(data => console.log(user + ": " + JSON.stringify(data) + "\n"))
+    // Query DB for users with linked accounts
+    conn.query(linked_accounts_sql, (err, results, fields) => {
+        results.forEach(link => {
+            console.log(link);
+            getDataForUser(link.vendor_id, link.username, link.oauth_token)
+                .then(data => processData(link.student_id, data))
             .catch(err => console.error("Error: " + err));
+        });
     });
 }
 
