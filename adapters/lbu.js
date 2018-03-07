dc34767a0084992170ad1c863e587378288cc60a
Jack Chapman <jackchapman1997@hotmail.co.uk>
2018-03-07 11:00:28 +0000

    additions to api

--- adapters/lbu.js
+++ adapters/lbu.js
@@ -8,17 +8,35 @@ const getCourseForUser = (user_id, auth_token) => {
 };
 
 const processCourses = (user_id, auth_token, courses) => {
-    let data = [];
-    let done = 0;
+    var data = [];
+    var done = 0;
     return new Promise((resolve, reject) => {
         courses.forEach(course => {
             const course_stats = {};
-            getCourseTags(user_id, course, auth_token)
-            .then(courses => {
+            getCourseTags(user_id, course, auth_token).then(modules => {
                 Object.assign(course_stats, {
                     id: course.id,
                     name: course.name,
+                    rating: course.rating,
+                    participant_count: course.participant_count,
+                    progress: student_course.progress,
+                    modules:{
+                        id:course_module.id,
+                        name:course_module.name,
+                        weighting:course_module.weighting,
+                        progress:student_course_module.progress                        
+                    }
                 });
+                course_stats['tags'] = modules;
+                Object.values(modules).forEach(module => {
+                    course_stats['module']+=progress;
+                });
+
+            }).then(_ => {
+                data.push(course_stats);
+                if (++done == courses.length) {
+                    resolve(data);
+                }
             });
         });
     });
